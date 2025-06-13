626ab0a2252fda21721b623c45c0dd0cda90379d
Isabel Figueroa Vargas <isabelfigueroavargas@outlook.com>
2025-06-13 16:37:23 -0400

    mensaje

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,11 @@
-const webpackConfig = require('./webpack.config');
+// const webpackConfig = require('./webpack.config');
 
 module.exports = function override(config) {
   // Agregar los externals de nuestro webpack.config.js
-  config.externals = {
-    ...config.externals,
-    ...webpackConfig.externals
-  };
-  
+  // config.externals = {
+  //   ...config.externals,
+  //   ...webpackConfig.externals
+  // };
+
   return config;
-}; 
+};
